5396c7612fb474a7f319d5aec7a6c05930137bd3
Augustine-edeh <info.augustinesedeh@gmail.com>
2025-01-21 22:34:06 +0100

    Initial commit-sending response

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  //   process.exit();
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<header><title>My Node Application</title></header>");
+  res.write("<body><h1>Hello Augustine. I am going to be great!</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
