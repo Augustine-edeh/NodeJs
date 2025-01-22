7fff53e1d83ad531f3873e604d8d77e8c36e4a34
Augustine-edeh <info.augustinesedeh@gmail.com>
2025-01-22 22:31:17 +0100

    Using the Node Modules System (module.exports)

---
+++ routes.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<header><title>Enter Message</title></header>");
+    res.write(
+      "<body><form action='/message' method='POST'><input placeholder='Please enter your message'type='text' name='message'/><button type=''>Send/Submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<header><title>My Node Application</title></header>");
+  res.write("<body><h1>Hello Augustine. I am going to be great!</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+module.exports = requestHandler;
