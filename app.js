578037c7489b9d73ca0984894727425ebbb99a93
Augustine-edeh <info.augustinesedeh@gmail.com>
2025-01-22 01:17:45 +0100

    redirecting routes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
   if (url === "/") {
     res.write("<html>");
     res.write("<header><title>Enter Message</title></header>");
@@ -9,6 +12,13 @@ const server = http.createServer((req, res) => {
       "<body><form action='/message' method='POST'><input placeholder='Please enter your message'type='text' name='message'/><button type=''>Send/Submit</button></form></body>"
     );
     res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.tsx", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
 
