6c2bb319070a06d401f41a9a4fa10623974de535
Augustine-edeh <info.augustinesedeh@gmail.com>
2025-01-22 01:03:06 +0100

    routing response

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  //   process.exit();
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<header><title>Enter Message</title></header>");
+    res.write(
+      "<body><form action='/message' method='POST'><input placeholder='Please enter your message'type='text' name='message'/><button type=''>Send/Submit</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
