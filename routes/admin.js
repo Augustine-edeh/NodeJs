e2e898e3f886378b02f6eeba904b0f2172e49253
Augustine-edeh <info.augustinesedeh@gmail.com>
2025-01-25 21:55:33 +0100

    chore: remove dead code

--- routes/admin.js
+++ routes/admin.js
@@ -9,10 +9,6 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "add-product.html"));
-
-  // res.sendFile(
-  //   "<form action='/admin/add-product' method='POST'><input type='text' name='title' /><button type='submit'>Submit</button></form>"
-  // );
 });
 
 // /admin/add-product => POST
