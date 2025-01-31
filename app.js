6c6cacf1e1643339819ac19dab67f9a8e72f76c0
Augustine-edeh <info.augustinesedeh@gmail.com>
2025-01-31 23:41:43 +0100

    Finishing the Pug Template

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "404-Not found" });
+  res.status(404).render("404", { pageTitle: "404-Page Not found" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -11,7 +11,7 @@ router.get("/", (req, res, next) => {
   const products = adminData.products;
   console.log(products);
 
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
