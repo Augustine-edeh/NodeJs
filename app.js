7aeb518bdf35bf75473c6c9284ef6fdb89ffcf05
Augustine-edeh <info.augustinesedeh@gmail.com>
2025-02-02 18:46:02 +0100

    fix: add `path` url parameter

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "404-Page Not found" });
+  res.status(404).render("404", { pageTitle: "404-Page Not found", path: "" });
 });
 
 app.listen(3000);
