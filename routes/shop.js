06353775bd6a356f07fa69ab2dbfb28321a85511
Augustine-edeh <info.augustinesedeh@gmail.com>
2025-02-03 22:27:16 +0100

    fix: remove unused imports

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,6 @@
-const path = require("path");
-
 const express = require("express");
 
-const rootDir = require("../util/path");
-
 const router = express.Router();
-const adminData = require("./admin");
 
 const productControllers = require("../controllers/products");
 
