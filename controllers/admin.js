99c89c33eeb0e1d34863c3794d1bf2b62a8fe541
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-04-18 19:15:52 +0100

    feat(deleting cart items): implement product deletion functionality and update cart management

--- controllers/admin.js
+++ controllers/admin.js
@@ -67,6 +67,6 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-
-  // const product = Product.findById(prodId);
+  Product.deleteById(prodId);
+  res.redirect("/admin/products");
 };
