13a5950627eaa2b7b50bbf47d46aee982b7884d5
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-04-30 00:49:35 +0100

    feat(fetching products): refactor product model to use database for fetching products

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ const shopRoutes = require("./routes/shop");
 
 const errorController = require("./controllers/error");
 
-db.execute("SELECT * FROM products")
-  .then((result) => console.log(result[0], result[1]))
-  .catch((err) => console.log(err));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
