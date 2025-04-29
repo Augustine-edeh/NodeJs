a87b907ff7e7dc22fe1d28fa4a244ff481497229
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-04-29 23:42:23 +0100

    feat(connecting our app ti the sql database): add MySQL connection pool and update dependencies

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const db = require("./util/database");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -12,6 +14,8 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const errorController = require("./controllers/error");
+
+db.execute("SELECT * FROM products");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
