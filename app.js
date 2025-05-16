0ffa41e97fc96b625a14415b4f3c4d813c6ebe19
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-05-17 00:04:44 +0100

    feat(Syncing JS Definitions to the Database): update database variable to sequelize and implement sync before server start

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -23,4 +23,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
